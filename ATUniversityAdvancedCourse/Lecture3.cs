8d720d04d8c909a1e6e5bd8e69f6f9d02c82a26c
Jeannine.Kwasnik <jeanninesworld@hotmail.com>
2021-11-14 12:20:49 -0500

    Lecture 3 Hands On

---
+++ ATUniversityAdvancedCourse/Lecture3.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Lecture3
+
+{
+
+    public delegate float Multiplication<in T1, in T2>(T1 subTotal, T2 taxPercentage);
+        private float TaxCalculation(decimal subTotal, decimal taxPercentage)
+        {
+            return (float)(subTotal * taxPercentage);
+        }
+        private float GetTotal(decimal subTotal, decimal tax)
+        {
+            return (float)(float)(subTotal + tax);
+        }
+    public void DelegateTest()
+        {
+        /*  Multiplication<float> handler = TaxCalculation;
+          Console.WriteLine(String.Format("The result is {0}", TaxCalculation(1, 2)));
+          Console.WriteLine(String.Format("The result is {0}", GetTotal(1, 2)));
+        */
+        Operations myOperation = new Operations();
+        Multiplication<decimal, decimal> TaxCalculation = myOperation.TaxCalculation;
+        Multiplication<decimal, decimal> GetTotal = myOperation.GetTotal;
+    }
+
+    [Test]
+    public void Collection()
+    {
+        Dictionary<int, decimal> result = new Dictionary<int, decimal>();
+        result.Add(100, (decimal).15);
+        result.Add(200, (decimal).17);
+        result.Add(300, (decimal).19);
+        result.Add(100, (decimal).15);
+        result.Add(100, (decimal).15);
+
+        
+
+        foreach (KeyValuePair<int,decimal> row in result)
+        {
+            Console.WriteLine("TaxCalculation = " + TaxCalculation(row.Key, row.Value));
+            Console.WriteLine("SubTotal = " + GetTotal(row.Key, row.Value));
+        }
+    }
+}
+    
---
+++ ATUniversityAdvancedCourse/Operations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+internal class Operations
+{
+    internal Lecture3.Multiplication<decimal, decimal> TaxCalculation;
+
+    public Lecture3.Multiplication<decimal, decimal> GetTotal { get; internal set; }
+
+    internal float GetTaxCalculation(decimal subTotal, decimal taxPercentage)
+    {
+        throw new NotImplementedException();
+    }
+}
